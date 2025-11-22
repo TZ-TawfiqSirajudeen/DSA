d89c48913dfb22da26e40590a615f19112a488af
tawfiq2725 <tawfiqsiraj003003@gmail.com>
2025-11-22 22:47:59 +0530

    Completed the loops

---
+++ basics/loops.js
@@ -0,0 +1,42 @@
+let n=5
+// star pattern 4X4
+
+// for(let i=0;i<4 ; i++){
+//     let row = ""
+//     for(let j=0;j<4;j++){
+//         row+="* "
+//     }
+//     console.log(row)
+// }
+
+/**
+ * 
+ * 
+ * *
+ * * *
+ * * * *
+ * 
+ * */ 
+
+
+// for(let i=0;i<n;i++){
+//     let row = "";
+
+//     // condition way changed based on j<i(this condtion will run the less than one by the n) and j<=i (exact what we give as n)
+//     for(let j=0;j<i;j++){
+//         row+="* "
+//     }
+//     console.log(row)
+// }
+
+// console.log(typeof(""+1))
+
+for(let i=1;i<=n;i++){
+    let row =" "
+
+    for(let j=1; j<i;j++){
+        row+=" "+j
+    }
+
+    console.log(row)
+}
