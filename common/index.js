ca9538873b2c4abab71d493af0051e1ef3cb165c
tawfiq2725 <tawfiqsiraj003003@gmail.com>
2025-11-24 21:11:01 +0530

    Sum of the digit done

---
+++ common/index.js
@@ -0,0 +1,26 @@
+// count digit
+
+// function countDigit(number) {
+//   // convert the number into string
+//   const num = number.toString().length;
+//   return num;
+// }
+
+// const number = 123;
+
+// console.log(countDigit(number));
+// function countDigit(number) {
+//  if(n===0) return 1
+//   let i = 0;
+//   while (number > 0) {
+//     // console.log(number, i);
+//     number = Math.floor(number / 10);
+//     i++;
+//   }
+
+//   return i;
+// }
+
+// const number = 123;
+
+// console.log(countDigit(number));
